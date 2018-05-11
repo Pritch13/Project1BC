f5aa3f9a7d21efa61f192c583ff8e09b8a4d352a
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-05-11 17:52:36 -0600

    ajax

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,17 +12,17 @@ function info() {
 $(document).ready(function () {
 
   $('.tabs').tabs();
-
   $('select').formSelect();
-
   $('.loading-screen').hide();
   $('.info').hide();
+
 
   $('.submit-button').on('click', function () {
     $('.welcome-screen').fadeOut(600);
     $('.loading-screen').fadeIn(600);
     setTimeout(loader, 4000);
     setInterval(info, 4000);
+
     var countrySelected = $('#options').val();
 
     $('.country-selection').text(countrySelected + '.');
@@ -33,20 +33,37 @@ $(document).ready(function () {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response){
-        $('#test4').append('<h3 >'+response.articles[0].title+'.</h3> <br> <p id="testtitle">'+response.articles[0].description+'</p>');
-        $('#test4').append('<h3 >'+response.articles[1].title+'.</h3> <br> <p id="testtitle">'+response.articles[1].description+'</p>');
-        $('#test4').append('<h3 >'+response.articles[2].title+'.</h3> <br> <p id="testtitle">'+response.articles[2].description+'</p>');
-      });
+    }).then(function (response) {
+      $('#test4').append('<h3 >' + response.articles[0].title + '.</h3> <br> <p id="testtitle">' + response.articles[0].description + '</p>');
+      $('#test4').append('<h3 >' + response.articles[1].title + '.</h3> <br> <p id="testtitle">' + response.articles[1].description + '</p>');
+      $('#test4').append('<h3 >' + response.articles[2].title + '.</h3> <br> <p id="testtitle">' + response.articles[2].description + '</p>');
+    });
 
-      var secondURL = 'https://www.reisewarnung.net/api?country=';
+    $.ajax({
+      url: 'https://restcountries.eu/rest/v2/name/'+ countrySelected ,
+      method: "GET",
+    }).then(function (data) {
+      console.log(data[0].alpha2Code);
+      
+      var cc = data[0].alpha2Code;
+
+
 
       $.ajax({
-        url: secondURL,
-        method: "GET"
-      }).then(function(response){
-        console.log(response);
-        });
+        url: 'https://api.tugo.com/v1/travelsafe/countries/' + cc,
+        method: "GET",
+        headers: {
+          'X-Auth-API-Key': 'wjmegnu9q3afgcrkhjbkxgpx',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (health) {
+        console.log(health);
+
+        $('#advisor').text(health.advisoryText + '!')
+        
+      });
+      
+    });
 
 
   });
