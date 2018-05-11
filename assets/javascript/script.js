1733d86402f34a740145fbde4125d7e6471ea0b5
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-05-11 10:58:22 -0600

    loader

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,32 @@
+  function loader() {
+    $('.loading-screen').hide();
+    // $('.info').hide();
+  }
 
+  function info() {
+    $('.info').fadeIn(800);
+  }
+
+
+  
+  
   $(document).ready(function(){
+
+    $('.tabs').tabs();
+
     $('select').formSelect();
+
+    $('.loading-screen').hide();
+    $('.info').hide();
+
+    $('.submit-button').on('click', function(){
+      $('.welcome-screen').fadeOut(600);
+      $('.loading-screen').fadeIn(600);
+      setTimeout(loader, 4000);
+      setInterval(info, 4000);
+    });
+
   });
+
+
+
